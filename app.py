75bc90ab052e920811045c882a2234c70dd34d40
Richard <richard@stromer.org>
2022-11-08 18:14:26 -0800

    build wrapper around np

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import pandas
 from flask import Flask, request, jsonify
-from neuralprophet import NeuralProphet
+from neuralprophet import NeuralProphet, set_log_level
 from flask_cors import CORS
+
+set_log_level("ERROR")
 
 app = Flask(__name__)
 CORS(app)
@@ -13,7 +16,33 @@ def hello_world():
 
 @app.route("/prediction", methods=["POST"])
 def prediction():
-    content = request.get_json(force=True)
-    # model = NeuralProphet()
-    # print(model)
-    return jsonify({"message": "Hello, World!"})
+    payload = request.get_json(force=True)
+    if "dataset" not in payload:
+        return jsonify({"error": "dataset is missing"}), 400
+    if "configuration" not in payload:
+        return jsonify({"error": "model configuration is missing"}), 400
+
+    dataset = payload["dataset"]
+    configuration = payload["configuration"]
+
+    df = pandas.DataFrame(dataset)
+    print(df.dtypes)
+    print(df.head())
+
+    # TODO: those transforms should not be necessary in the future
+    df = df.dropna()
+    df["ds"] = pandas.to_datetime(df["ds"])
+    df["y"] = pandas.to_numeric(df["y"])
+
+    model = NeuralProphet(epochs=1)
+    metrics = model.fit(df, freq="D")
+
+    forecast = model.predict(df)
+
+    return jsonify(
+        {
+            "status": "ok",
+            "metrics": metrics.to_dict(),
+            "forecast": forecast.to_dict("records"),
+        }
+    )
