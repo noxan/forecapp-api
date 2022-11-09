c12e6b2db39be4d09be104119c33d1d8fd7f991c
Richard <richard@stromer.org>
2022-11-08 17:54:29 -0800

    install flask cors

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import json
+from flask import Flask, request, jsonify
+from neuralprophet import NeuralProphet
 
 app = Flask(__name__)
 
@@ -6,3 +8,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route("/prediction", methods=["POST"])
+def prediction():
+    content = request.get_json(force=True)
+    # model = NeuralProphet()
+    # print(model)
+    return jsonify({"message": "Hello, World!"})
