627653f68bfb87805b5751b9a8ed0dd865d4551d
Richard <richard@stromer.org>
2022-11-10 15:33:50 -0800

    install cdktf aws provider

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 from constructs import Construct
 from cdktf import App, TerraformStack
+from cdktf_cdktf_provider_aws import AwsProvider
 
 
 class MyStack(TerraformStack):
     def __init__(self, scope: Construct, id: str):
         super().__init__(scope, id)
 
-        # define resources here
+        AwsProvider(self, "aws", region="us-west-1", profile="forecapp")
 
 
 app = App()
