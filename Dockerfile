4d6e86743a2550e505a69e4aa46f7b00aa7951f8
Richard <richard@stromer.org>
2022-11-10 12:24:15 -0800

    get docker setup working

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,9 @@
-FROM python:3.10-slim
+FROM public.ecr.aws/lambda/python:3.9
 
-WORKDIR /app
 
-COPY requirements.txt .
-RUN pip install -r requirements.txt
+COPY requirements.txt  .
+RUN pip3 install -r requirements.txt --target "${LAMBDA_TASK_ROOT}"
 
-COPY . .
+COPY app.py ${LAMBDA_TASK_ROOT}
 
-RUN ["gunicorn app:app"]
+CMD ["gunicorn app:app"]
--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+aws ecr-public --profile forecapp get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+
 docker build -t forecapp-api .
+
+aws ecr --profile forecapp get-login-password --region us-west-1 | docker login --username AWS --password-stdin 719834076613.dkr.ecr.us-west-1.amazonaws.com
+
+docker tag forecapp-api:latest 719834076613.dkr.ecr.us-west-1.amazonaws.com/forecapp-api:latest
+docker push 719834076613.dkr.ecr.us-west-1.amazonaws.com/forecapp-api:latest
