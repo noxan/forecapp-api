32eca2dfcc74c2ed8de8d5ad67890943cacb3198
Richard <richard@stromer.org>
2022-11-10 17:54:21 -0800

    create ecs cluster with cdktf

--- main.ts
+++ main.ts
@@ -3,12 +3,25 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import { EcsCluster } from "@cdktf/provider-aws/lib/ecs-cluster";
+
+const region = "us-west-1";
+const profile = "forecapp";
+const name = "forecapp-api";
+const tags = {
+  project: "forecapp-api",
+};
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    new AwsProvider(this, "aws", { region: "us-west-1" });
+    new AwsProvider(this, "aws", { region, profile });
+
+    new EcsCluster(this, `ecs-${name}`, {
+      name,
+      tags,
+    });
   }
 }
 
