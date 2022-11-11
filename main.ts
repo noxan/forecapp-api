7c031f3455dd8ec24194ecbe6a8d4959f12ddf20
Richard <richard@stromer.org>
2022-11-10 18:04:15 -0800

    add basic ecs task definition

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
 import { EcsCluster } from "@cdktf/provider-aws/lib/ecs-cluster";
+import { EcsTaskDefinition } from "@cdktf/provider-aws/lib/ecs-task-definition";
 
 const region = "us-west-1";
 const profile = "forecapp";
@@ -20,6 +21,22 @@ class MyStack extends TerraformStack {
 
     new EcsCluster(this, `ecs-${name}`, {
       name,
+      tags,
+    });
+
+    new EcsTaskDefinition(this, `ecs-task-${name}`, {
+      family: "service",
+      containerDefinitions: JSON.stringify([
+        {
+          name,
+          portMappings: [
+            {
+              containerPort: 80,
+              hostPort: 80,
+            },
+          ],
+        },
+      ]),
       tags,
     });
   }
