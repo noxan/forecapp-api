c909bc14d89e8fcc9058024e2d7e825003a5c715
Richard <richard@stromer.org>
2022-12-21 17:11:51 -0800

    create gunicorn config

---
+++ gunicorn.conf.py
@@ -0,0 +1,8 @@
+# Access log - records incoming HTTP requests
+accesslog = "/var/log/gunicorn.access.log"
+# Error log - records Gunicorn server goings-on
+errorlog = "/var/log/gunicorn.error.log"
+# Whether to send Django output to the error log
+capture_output = True
+# How verbose the Gunicorn error logs should be
+loglevel = "info"
