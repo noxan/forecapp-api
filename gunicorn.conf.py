0eb5accfee57c978685dc3f605d7f63756c2d6c3
Richard <richard@stromer.org>
2023-01-10 14:47:08 +0100

    adjust gunicorn config for aws

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,16 +1,16 @@
 # Access log - records incoming HTTP requests
-accesslog = "logs/gunicorn.access.log"
+# accesslog = "logs/gunicorn.access.log"
 # Error log - records Gunicorn server goings-on
-errorlog = "logs/gunicorn.error.log"
+# errorlog = "logs/gunicorn.error.log"
 # Whether to send Django output to the error log
-capture_output = True
+# capture_output = True
 # How verbose the Gunicorn error logs should be
-loglevel = "info"
+# loglevel = "info"
 
 wsgi_app = "main:app"
 
 # Workers
-workers = 1
+workers = 2
 
 graceful_timeout = 360
 timeout = 360
